a2a3abf52b065c4029c718a02c93f9ac4f4345ea
Ashwin Sathian <ashwinsathian@Ashwins-MacBook-Air.local>
2019-11-20 14:13:59 +0400

    Version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
-  navLinks: Array<object>;
+export class AppComponent {
 
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-    this.navLinks = [
-      { name: 'Home', path: '/' },
-      { name: 'About', path: '/about' },
-      { name: 'Gallery', path: '/gallery' },
-      { name: 'Contact', path: '/contact' }
-    ];
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
 }
